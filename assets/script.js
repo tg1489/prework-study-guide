85eff28864b10e6d56c190ad689cf819dba474ab
Tony <tonyguarino1489@gmail.com>
2023-02-08 15:16:27 -0500

    Created JavaScript code

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+let shapes = ['triangle', 'square', 'pentagon', 'circle'];
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if ("Let's study JavaScript!");
+else {
+    console.log('Please try again!');
+}
+*/
+
+
+for (let x = 0; x< shapes.length; x++) {
+    console.log(shapes[x]);
+}
+
+for (let x=0; x<topics.length; x++) {
+    console.log(topics[x]);
+}
