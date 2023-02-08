98fe497971283b7726bd31536a07bcc64db54d81
Tony <tonyguarino1489@gmail.com>
2023-02-08 15:55:03 -0500

    Added JavaScript code and notes inside the html file

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
 let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 let shapes = ['triangle', 'square', 'pentagon', 'circle'];
+let randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-/*
-if (topic === 'HTML') {
+
+
+function selectTopic() {
+if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
-} else if ("Let's study JavaScript!");
+} else if (randomTopic === 'JavaScript'){
+    console.log("Let's study JavaScript!")
+}
+ 
 else {
     console.log('Please try again!');
+    }
 }
-*/
 
 
 for (let x = 0; x< shapes.length; x++) {
@@ -22,3 +28,18 @@ for (let x = 0; x< shapes.length; x++) {
 for (let x=0; x<topics.length; x++) {
     console.log(topics[x]);
 }
+
+function helloWorld () {
+    console.log('Hello, world, I am a function!')
+}
+
+function listTopics () {
+    for (let x=0; x<topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
+
+console.log('Here are the topics we learned through Prework: ')
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
